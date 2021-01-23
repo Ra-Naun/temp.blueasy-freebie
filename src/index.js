c7b0957729cb2ec9607070cb0e6e0d0fc05a2dbe
Dmitry <dmitriy_slavskiy@mail.ru>
2021-01-23 15:43:09 +0400

    add header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import * as $ from 'jquery';
 import './styles/styles.css';
+import './styles/style.scss';
+
+const navBar__links = $('.nav-bar__link');
+navBar__links[0].classList.add('active');
+
+const selectTab = (e) => {
+  [].forEach.call(navBar__links, (link) => {
+    link.classList.remove('active');
+  });
+  e.target.classList.add('active');
+};
+
+[].forEach.call(navBar__links, (link) => {
+  link.addEventListener('click', selectTab);
+});
